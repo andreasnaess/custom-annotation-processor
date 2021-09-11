c94a35019e6b8a218bc5d2a6e2be6ae744cd713a
Andreas Næss <andreas.n92@gmail.com>
2021-09-11 14:39:21 +0200

    Lagt til logging av parametere

--- src/main/kotlin/no/dreamstate/customannotationprocessor/AtsValidateAspect.kt
+++ src/main/kotlin/no/dreamstate/customannotationprocessor/AtsValidateAspect.kt
@@ -1,7 +1,9 @@
 package no.dreamstate.customannotationprocessor
 
+import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.reflect.CodeSignature
 import org.springframework.stereotype.Component
 
 @Aspect
@@ -9,7 +11,11 @@ import org.springframework.stereotype.Component
 class AtsValidateAspect(private val helloService: HelloService) {
 
     @Before(value = "@annotation(atsValidate)")
-    fun validate(atsValidate: AtsValidate) {
+    fun validate(joinPoint: JoinPoint, atsValidate: AtsValidate) {
+        (joinPoint.signature as CodeSignature).parameterNames.forEach {
+            println(it)
+        }
+        println(joinPoint.args[0])
         if (atsValidate.rettighet == Rettighet.LES) {
             helloService.les()
         } else if (atsValidate.rettighet == Rettighet.SKRIV) {
--- src/main/kotlin/no/dreamstate/customannotationprocessor/HelloController.kt
+++ src/main/kotlin/no/dreamstate/customannotationprocessor/HelloController.kt
@@ -2,15 +2,16 @@ package no.dreamstate.customannotationprocessor
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
 class HelloController {
 
-    @AtsValidate(rettighet = Rettighet.SKRIV)
+    @AtsValidate(rettighet = Rettighet.LES)
     @GetMapping("/hello")
-    fun hello(): String {
-        return "Hello world"
+    fun hello(@RequestParam(required = false) name: String?): String {
+        return "Hello ${name ?: "world"}"
     }
 }
